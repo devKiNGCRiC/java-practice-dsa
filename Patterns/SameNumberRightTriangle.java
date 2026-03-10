a7e445643795884a42c3126285cc3943ffd2661e
devKiNGCRiC <kingcric28@gmail.com>
2026-03-10 22:42:24 +0530

    Number Right Triangle Pattern problems

---
+++ Patterns/SameNumberRightTriangle.java
@@ -0,0 +1,11 @@
+public class SameNumberRightTriangle {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++ ){
+            for(int j = 1; j < i+1 ; j++){
+                int k = i;
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
