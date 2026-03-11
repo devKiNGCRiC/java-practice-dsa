202f7c4821fb490d8d6ce8358cdc07bc147ecc30
devKiNGCRiC <kingcric28@gmail.com>
2026-03-11 19:06:39 +0530

    Inverted Pyramid Pattern Problem

--- Patterns/InvertedPyramid.java
+++ Patterns/InvertedPyramid.java
@@ -1,7 +1,7 @@
-public class Pyramid {
+public class InvertedPyramid {
     public static void main(String[] args) {
         int n = 5;
-        for(int i=1; i<=n;i++){
+        for(int i=n; i>=1;i--){
             //spaces
             for(int j = 1; j<=n-i; j++){
                 System.out.print(" ");
