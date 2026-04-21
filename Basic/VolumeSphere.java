032e6347349bd8ba2ca96cef5d1c085c7aa779f7
devKiNGCRiC <kingcric28@gmail.com>
2026-04-21 23:09:26 +0530

    Volume Of Sphere

---
+++ Basic/VolumeSphere.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class VolumeSphere {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter The Radius Value : ");
+        double radius = input.nextDouble();
+        double volume = 4* (Math.PI * Math.pow(radius,3))/3;
+        System.out.print("The Area Of Sphere is : " + volume);
+        input.close();
+    }
+}
